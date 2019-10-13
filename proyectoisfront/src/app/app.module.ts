1a3103a5426ffc3fa313e046711a500e7a26dc85
CarMer194 <00060313@uca.edu.sv>
2019-10-12 18:18:34 -0600

    Agregando el menu

--- proyectoisfront/src/app/app.module.ts
+++ proyectoisfront/src/app/app.module.ts
@@ -1,37 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSidenavModule, MatListModule, MatIconModule, MatButtonModule, MatAutocompleteModule, MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDialogModule, MatDividerModule, MatExpansionModule, MatGridListModule, MatInputModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule, MatRippleModule, MatSelectModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatStepperModule, MatTableModule, MatTabsModule, MatTooltipModule, MAT_DIALOG_DEFAULT_OPTIONS} from '@angular/material'import { AppRoutingModule } from './app-routing.module';
+import {MatSidenavModule, MatListModule, MatIconModule, MatButtonModule, MatAutocompleteModule, MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDialogModule, MatDividerModule, MatExpansionModule, MatGridListModule, MatInputModule, MatMenuModule, MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule, MatRippleModule, MatSelectModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatStepperModule, MatTableModule, MatTabsModule, MatTooltipModule, MAT_DIALOG_DEFAULT_OPTIONS, MatToolbarModule} from '@angular/material';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AnimalesComponent } from './animales/animales.component';
 import { HttpClientModule }  from '@angular/common/http';
 import {IsapiService} from './shared/isapi.service';
 import { AnimalComponent } from './animal/animal.component';
-<<<<<<< HEAD
-import { MenudemoComponent } from './menudemo/menudemo.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import {FlexLayoutModule} from "@angular/flex-layout";
-=======
 import { ProveedoresComponent } from './proveedores/proveedores.component';
 import { ProveedorComponent } from './proveedor/proveedor.component'
 import { ProductosComponent } from './productos/productos.component';
 import { ProductoComponent } from './producto/producto.component'
+import { FormsModule } from '@angular/forms';
 
->>>>>>> sprint2
 
 @NgModule({
   declarations: [
     AppComponent,
     AnimalesComponent,
     AnimalComponent,
-<<<<<<< HEAD
-    MenudemoComponent
-=======
     ProveedoresComponent,
     ProveedorComponent,
     ProductosComponent,
     ProductoComponent
->>>>>>> sprint2
   ],
   imports: [
     BrowserModule,
